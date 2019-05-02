e5eff4a15af14acd35667d293ea23d65aee873de
Arthur Sousa <artosousa@gmail.com>
2019-05-02 15:34:33 -0400

    adding ternary operators
    handle the undefined data with ternary operators

--- js/script.js
+++ js/script.js
@@ -22,12 +22,17 @@ fetch(url)
         return fetch(newUrl);
     })
     .then((resp) => resp.json()).then((data) => {
+        const totalKills = data.overallData.defaultModes.kills;
+        const matchesPlayed = data.overallData.defaultModes.matchesplayed;
+        const matchesWon = data.overallData.defaultModes.placetop1;
+        const playersOutlived = data.overallData.defaultModes.playersoutlived; 
+        const score = data.overallData.defaultModes.score;
         console.log(data.overallData.defaultModes);
-        document.getElementById("total-kills").innerHTML = data.overallData.defaultModes.kills;
-        document.getElementById("matches-played").innerHTML = data.overallData.defaultModes.matchesplayed;
-        document.getElementById("placed-top").innerHTML = data.overallData.defaultModes.placetop1;
-        document.getElementById("players-outlived").innerHTML = data.overallData.defaultModes.playersoutlived;
-        document.getElementById("overall-score").innerHTML = data.overallData.defaultModes.score;
+        document.getElementById("total-kills").innerHTML = typeof totalKills === 'undefined' ? 'You have 0 kills' : totalKills;
+        document.getElementById("matches-played").innerHTML = typeof matchesPlayed === 'undefined' ? 'You have played 0 matches' : matchesPlayed;
+        document.getElementById("placed-top").innerHTML = typeof matchesWon === 'undefined' ? 'You have won 0 matches' : matchesWon;
+        document.getElementById("players-outlived").innerHTML = typeof playersOutlived === 'undefined' ? 'You have outplayed 0 players' : playersOutlived;
+        document.getElementById("overall-score").innerHTML = typeof score === 'undefined' ? '0' : score;
     }).catch(function(error) {
         console.log('request failed', error);
     });
