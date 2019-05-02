a86d7d3e0e4ef8aa63acf442ba2660591c2a4b39
Arthur Sousa <artosousa@gmail.com>
2019-05-02 14:54:42 -0400

    Form Fixed
    added functionality to the form

--- js/script.js
+++ js/script.js
@@ -3,36 +3,36 @@ const baseUrl = 'https://fortnite-public-api.theapinetwork.com/prod09/users';
 
 const inputField = document.getElementById("epic-username");
 const submitBtn = document.getElementById("user-submit");
-
+const form = document.getElementById("form");
 
 const getUserStats = (event) => {
-    // to prevent the regular form submit behavior
-    event.preventDefault();
-    const username = event.target.username.value;
-  
-    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-    let url = `${baseUrl}/id?username=${username}` ;
-    console.log(proxyUrl + url);
-    fetch(url)
-        .then((resp) => resp.json())
-        .then((data) => {
-            const userId = data.uid;
-            let newUrl = `${baseUrl}/public/br_stats_v2?user_id=${userId}`;
-      
-            return fetch(newUrl);
-        })
-        .then((resp) => resp.json()).then((data) => {
-            console.log(data.overallData.defaultModes);
-            document.getElementById("total-kills").innerHTML = data.overallData.defaultModes.kills;
-            document.getElementById("matches-played").innerHTML = data.overallData.defaultModes.matchesplayed;
-            document.getElementById("placed-top").innerHTML = data.overallData.defaultModes.placetop1;
-            document.getElementById("players-outlived").innerHTML = data.overallData.defaultModes.playersoutlived;
-            document.getElementById("overall-score").innerHTML = data.overallData.defaultModes.score;
-        }).catch(function(error) {
-            console.log('request failed', error);
-        });
-  }
+// to prevent the regular form submit behavior
+event.preventDefault();
+const username = event.target.username.value;
+
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+let url = `${baseUrl}/id?username=${username}` ;
+console.log(proxyUrl + url);
+fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+        const userId = data.uid;
+        let newUrl = `${baseUrl}/public/br_stats_v2?user_id=${userId}`;
+    
+        return fetch(newUrl);
+    })
+    .then((resp) => resp.json()).then((data) => {
+        console.log(data.overallData.defaultModes);
+        document.getElementById("total-kills").innerHTML = data.overallData.defaultModes.kills;
+        document.getElementById("matches-played").innerHTML = data.overallData.defaultModes.matchesplayed;
+        document.getElementById("placed-top").innerHTML = data.overallData.defaultModes.placetop1;
+        document.getElementById("players-outlived").innerHTML = data.overallData.defaultModes.playersoutlived;
+        document.getElementById("overall-score").innerHTML = data.overallData.defaultModes.score;
+    }).catch(function(error) {
+        console.log('request failed', error);
+    });
+}
 
 
-
+form.addEventListener('submit', getUserStats);
   
